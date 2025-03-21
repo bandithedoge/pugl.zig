198ba88f76a38a38a8e8ec8a3c211c7e99005e97
bandithedoge <bandithedoge@protonmail.com>
2025-03-21 11:08:58 +0100

    flake: don't use zig2nix (again)

--- flake.nix
+++ flake.nix
@@ -2,20 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    zig.url = "github:Cloudef/zig2nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: {
-        devShells.default = pkgs.mkShell {
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell.override {stdenv = pkgs.zigStdenv;} {
           nativeBuildInputs = with pkgs; [
-            inputs.zig.packages.${system}.zig-0_14_0
+            zig
             zls
           ];
 
