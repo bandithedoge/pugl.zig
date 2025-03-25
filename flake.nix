b78677810c14c51fbf21382d8ce1d6c4e2686c36
bandithedoge <bandithedoge@protonmail.com>
2025-03-25 22:36:20 +0100

    ci: build everything with nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    zig2nix.url = "github:Cloudef/zig2nix";
   };
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      perSystem = {pkgs, ...}: {
-        devShells.default = pkgs.mkShell.override {stdenv = pkgs.zigStdenv;} {
+      perSystem = {
+        pkgs,
+        system,
+        self',
+        ...
+      }: let
+        zigEnv = inputs.zig2nix.zig-env.${system} {};
+      in {
+        packages.default = zigEnv.package {
+          src = pkgs.lib.cleanSource ./.;
+
           nativeBuildInputs = with pkgs; [
-            zig
-            zls
+            python3
           ];
 
           buildInputs = with pkgs; [
+            cairo
             libGL
             pkg-config
             vulkan-loader
@@ -23,6 +33,23 @@
             xorg.libXext
             xorg.libXrandr
           ];
+
+          zigBuildFlags = [
+            "docs"
+            "-Dopengl=true"
+            "-Dvulkan=true"
+            "-Dcairo=true"
+            "-Dbuild_cairo=false"
+            "-Dstub=true"
+          ];
+        };
+
+        devShells.default = zigEnv.mkShell {
+          nativeBuildInputs = with pkgs; [
+            zls
+          ];
+
+          inherit (self'.packages.default) buildInputs;
         };
       };
     };
