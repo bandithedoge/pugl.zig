ca6300ead06ed70859a05731420348d0415c206d
nat3 <billing.nate@gmx.de>
2026-08-28 20:49:49 +0200

    lean-deps: link CoreVideo framework on macos, pin pugl-c to git fork

--- build.zig
+++ build.zig
@@ -97,6 +97,7 @@ pub fn build(b: *std.Build) !void {
         },
         .mac => {
             pugl.linkFramework("Cocoa", .{});
+            pugl.linkFramework("CoreVideo", .{});
         },
         .win => {
             try c_flags.appendSlice(b.allocator, &.{
